d64f581bfe630c698f0aad8c36c77a01d5936c14
MikeNewsholme <100385842+MikeNewsholme@users.noreply.github.com>
2022-10-18 21:30:59 -0400

    please work

---
+++ petfax/pet.py
@@ -0,0 +1,11 @@
+from flask import (Blueprint, render_template)
+
+bp = Blueprint('pet', __name__, url_prefix="/pets")
+
+import json
+pets = json.load(open('pets.json'))
+print(pets)
+
+@bp.route('/')
+def index():
+    return render_template('index.html', pets=pets)
