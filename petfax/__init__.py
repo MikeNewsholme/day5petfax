85c0f21f457e4453b923aac1ef306e7bee72e7f6
MikeNewsholme <100385842+MikeNewsholme@users.noreply.github.com>
2022-10-20 10:27:13 -0400

    finished facts and show

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -10,4 +10,7 @@ def create_app():
     from . import pet
     app.register_blueprint(pet.bp)
 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
     return app
--- petfax/pet.py
+++ petfax/pet.py
@@ -8,4 +8,9 @@ print(pets)
 
 @bp.route('/')
 def index():
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
+
+@bp.route('/<int:id>')
+def show(id):
+    pet = pets[id - 1]
+    return render_template('pets/show.html', pet=pet)
